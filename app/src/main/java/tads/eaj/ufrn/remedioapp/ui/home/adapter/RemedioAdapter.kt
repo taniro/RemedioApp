7f36b32f3bbd8c11d79162ce519a1cf443d5ee7b
tanir <tanirocr@gmail.com>
2022-11-24 00:47:41 -0300

    recyclerview

--- app/src/main/java/tads/eaj/ufrn/remedioapp/ui/home/adapter/RemedioAdapter.kt
+++ app/src/main/java/tads/eaj/ufrn/remedioapp/ui/home/adapter/RemedioAdapter.kt
@@ -1,32 +1,42 @@
 package tads.eaj.ufrn.remedioapp.ui.home.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import tads.eaj.ufrn.remedioapp.R
+import tads.eaj.ufrn.remedioapp.databinding.RemedioListItemBinding
 import tads.eaj.ufrn.remedioapp.model.Remedio
 
 class RemedioAdapter: ListAdapter<Remedio, RemedioAdapter.RemedioViewHolder> (RemedioDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RemedioViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.remedio_list_item, parent, false)
-        return RemedioViewHolder(view)
+        return RemedioViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: RemedioViewHolder, position: Int) {
-        holder.nomeTextView.text = currentList[position].nome
-        holder.dosagemTextView.text = currentList[position].dosagem.toString()
+        val remedio = getItem(position)
+        holder.bind(remedio)
     }
 
 
-    class RemedioViewHolder(v:View): RecyclerView.ViewHolder(v) {
-        var nomeTextView = v.findViewById<TextView>(R.id.textViewNome)
-        var dosagemTextView = v.findViewById<TextView>(R.id.textViewDosagem)
+    class RemedioViewHolder private constructor(var binding: RemedioListItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(remedio: Remedio) {
+            binding.textViewNome.text = remedio.nome
+            binding.textViewDosagem.text = remedio.dosagem.toString()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): RemedioViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = RemedioListItemBinding.inflate(inflater, parent, false)
+                return RemedioViewHolder(binding)
+            }
+        }
     }
+
+
 }
 
 class RemedioDiffCallback : DiffUtil.ItemCallback<Remedio>() {
